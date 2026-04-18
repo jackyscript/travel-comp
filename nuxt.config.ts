5c711229a430cc2880dccca66c0099309bd3b8fa
jackyscript <8930712+jackyscript@users.noreply.github.com>
2026-04-19 00:25:19 +0200

    add deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,14 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module']
+  modules: ['vuetify-nuxt-module'],
+  app: {
+    head: {
+      title: 'Travel Comp', // default fallback title
+      htmlAttrs: {
+        lang: 'en',
+      },
+    },
+    baseURL: "/travel-comp/",
+  },
 })
